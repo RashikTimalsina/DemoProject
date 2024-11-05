38fc0daef00e072b7ee5e29487dc6ec66a3f8dcd
RashikTimalsina <rashiktimalsina21@gmail.com>
2024-11-05 18:42:26 +0545

    changes being add

---
+++ src/com/DemoProject/Double.java
@@ -0,0 +1,18 @@
+package com.DemoProject;
+
+public class Double {
+
+    // a mile is equal to 1.5033 kilometer
+
+    public static void main(String[] args) {
+
+        double PI=3.14159265;
+        int radius=20;
+        double area = PI * radius * radius;
+        System.out.println(area);
+
+
+
+    }
+
+}
---
+++ src/com/DemoProject/Identation.java
@@ -0,0 +1,16 @@
+package com.DemoProject;
+
+ class Identation {
+
+    public static void main(String[] args) {
+
+        int myVariable=50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                "identation" + 
+                "check");
+    }
+}
