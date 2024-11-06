777f081bb49b60e5e5f89a0cbc8be2e871f50ab2
RashikTimalsina <rashiktimalsina21@gmail.com>
2024-11-06 15:46:33 +0545

    new commit changes

--- src/com/rashiktimalsina/CodeBlocks.java
+++ src/com/rashiktimalsina/CodeBlocks.java
@@ -4,29 +4,13 @@ public class CodeBlocks {
 
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score=5000;
-        int levelCompleted=5;
-        int bonus=50;
+        calculateScore(true, 5000, 5, 50);
+        calculateScore(true, 10000, 7, 100);
 
-        calculateScore();
-
-        score=10000;
-        levelCompleted=8;
-        bonus=40;
-
-        if(gameOver){
-            int finalScore=score+(levelCompleted*bonus);
-            System.out.println("your final score is : " + finalScore);
-        }
     }
 
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
-    public static void calculateScore(){
-        boolean gameOver = true;
-        int score=5000;
-        int levelCompleted=5;
-        int bonus=50;
 
 //        if(score <=5000 && score >=1000){
 //
--- src/com/rashiktimalsina/HelloWorld.java
+++ src/com/rashiktimalsina/HelloWorld.java
@@ -1,3 +1,5 @@
+package com.rashiktimalsina;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
