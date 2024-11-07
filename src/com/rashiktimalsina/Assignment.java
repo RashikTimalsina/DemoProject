f26582781fbcac032125766121c3c9138f2bf22e
RashikTimalsina <rashiktimalsina21@gmail.com>
2024-11-07 13:17:11 +0545

    comment being add

---
+++ src/com/rashiktimalsina/Assignment.java
@@ -0,0 +1,60 @@
+package com.rashiktimalsina;
+
+//
+//Create a method called displayHighScorePosition
+//it should contain players name as a parameter , and a 2nd parameter as a position in the high score table
+//        You should display the players name along with a message like "managed to get into position " and the
+//        position they got and a further message on the high score table
+//
+//        Create a 2nd method called calculateHighScorePosition
+//        it should be sent one argument only, the player score
+//        should be return an int
+//        the return data should be
+//        1.if the score is >1000
+//        2.if the score is >500 and <1000
+//        3.if the score is >100 and <500
+//        4. in all other cases
+//        //call both methods and display the results of the following
+//        // a score of 1500, 900 400 and 50
+
+public class Assignment {
+    public static void main(String[] args) {
+
+        int highScorePosition=calculateHighScorePosition(1500);
+        displayHighScorePosition("Rashik", highScorePosition);
+
+        highScorePosition=calculateHighScorePosition(900);
+        displayHighScorePosition("Ram", highScorePosition);
+
+
+         highScorePosition=calculateHighScorePosition(400);
+        displayHighScorePosition("Raj", highScorePosition);
+
+
+         highScorePosition=calculateHighScorePosition(50);
+        displayHighScorePosition("Rashik", highScorePosition);
+    }
+
+    public static void displayHighScorePosition(String pl_name, int highScorePosition) {
+        System.out.println(pl_name + " managed to get into position "
+                + highScorePosition + " on the high score table ");
+
+    }
+
+    public static int calculateHighScorePosition(int pl_score){
+
+        if(pl_score > 1000){
+            return 1;
+        }
+        else if(pl_score > 500 && pl_score < 1000){
+            return 2;
+        }
+        else if(pl_score > 100 && pl_score < 500){
+            return 3;
+        }
+        else{
+            return 4;
+        }
+    }
+
+}
--- src/com/rashiktimalsina/CodeBlocks.java
+++ src/com/rashiktimalsina/CodeBlocks.java
@@ -4,32 +4,22 @@ public class CodeBlocks {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 5000, 5, 50);
-        calculateScore(true, 10000, 7, 100);
+        int highScore=calculateScore(true, 5000, 5, 50);
+        System.out.println("your final score is: " + highScore);
+
+        highScore=calculateScore(true, 10000, 10, 100);
+        System.out.println("your final score is: " + highScore);
 
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
-
-//        if(score <=5000 && score >=1000){
-//
-//            System.out.println("your score was less or equal than 5000 and more or equal than 1000");
-//        }
-//        else if(score <1000){
-//
-//            System.out.println("your score was lesser than 1000");
-//        }
-//        else{
-//
-//            System.out.println("got here");
-//
-//        }
-
-        if(gameOver){
-            int finalScore=score+(levelCompleted*bonus);
-            System.out.println("your final score is : " + finalScore);
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += score;
+            return finalScore;
         }
+        return -1;
     }
 
 }
