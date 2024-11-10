769e6edd2e2704652c258e85d6ebc73b59959592
RashikTimalsina <rashiktimalsina21@gmail.com>
2024-11-10 13:57:16 +0545

    commit update

---
+++ src/com/rashiktimalsina/WhileLoop.java
@@ -0,0 +1,22 @@
+package com.rashiktimalsina;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int count =0;
+        while(count != 5) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        count=1;
+        while(true){
+            if(count == 5) {
+                break;
+            }
+            System.out.println("Count value is " + count);
+            count++;
+
+        }
+    }
+}
