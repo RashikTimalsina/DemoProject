c82984251433a513edd39640168caa718cf11320
RashikTimalsina <rashiktimalsina21@gmail.com>
2024-11-10 13:55:53 +0545

    commit update

---
+++ src/com/rashiktimalsina/DoWhile.java
@@ -0,0 +1,13 @@
+package com.rashiktimalsina;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int count =1;
+        do{
+            System.out.println(count);          //At least once the println is executed
+            count++;
+        }while(count<=10);
+
+    }
+}
